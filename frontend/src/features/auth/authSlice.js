ee290bd1c53fde11b1e4e26dc5bd54be6ab7c82e
panosjapan7 <panosjapan@gmail.com>
2022-07-05 08:34:07 +0200

    Logout user

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 
+// Get user from local storage
 const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
@@ -22,10 +23,15 @@ export const register = createAsyncThunk("auth/register", async (user, thunkAPI)
     }
 })
 
-//Login user
+// Login user
 export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
     console.log(user);
 })
+
+// Logout user
+export const logout = createAsyncThunk("auth/logout", async () => {
+    await authService.logout();
+});
 
 export const authSlice = createSlice({
     name: "auth",
@@ -51,6 +57,9 @@ export const authSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
+                state.user = null;
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null;
             })
     }
