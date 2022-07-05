dacbc60484b8f01e9f79bddd63c468143f6c1750
panosjapan7 <panosjapan@gmail.com>
2022-07-05 13:54:56 +0200

    Create Ticket Functionality

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,20 +1,48 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 function NewTicket() {
     const { user } = useSelector((state) => state.auth);
+    const { isLoading, isError, isSuccess, message } = useSelector((state) => state.tickets);
+
     const [name] = useState(user.name);
     const [email] = useState(user.email);
     const [product, setProduct] = useState("iPhone");
     const [description, setDescription] = useState("");
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        if (isSuccess) {
+            dispatch(reset());
+            navigate("/tickets");
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, message, navigate, dispatch]);
+
     const onSubmit = (e) => {
         e.preventDefault();
+        dispatch(createTicket({ product, description }));
+    }
 
+    if (isLoading) {
+        return <Spinner />
     }
 
     return (
     <>
+        <BackButton url="/" />
         <section className="heading">
             <h1>New Ticket</h1>
             <p>Please fill out the form below</p>
