e946063be31dc7f2ed14aaec74c859822870647a
panosjapan7 <panosjapan@gmail.com>
2022-07-05 09:34:49 +0200

    Ticket model and Routes

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Welcome to the Suport Desk API"
-    });
+    res.status(200).json({message: "Welcome to the Suport Desk API"});
 })
 
 // Routes
-app.use("/api/users/", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
